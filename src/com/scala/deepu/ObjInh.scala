eda09996a9a67015db781d4ee7a37228bd33cdf3
Deepu Raj <Deepuraj_analytics@outlook.com>
2016-11-22 21:54:25 +1100

    Concreate Implementation

--- src/com/scala/deepu/ObjInh.scala
+++ src/com/scala/deepu/ObjInh.scala
@@ -1,12 +1,12 @@
-/*package com.scala.deepu
-*//**
+package com.scala.deepu
+/**
  * @author rajde
- *//*
+ */
 
-abstract class amy(val age: Int)
+abstract class amy( val age: Int)
 
-class catty (val age : Int, val tailSize: Double) extends amy(age)
-class doggy (val age : Int, val earlSize: Double) extends amy(age)
+class catty (override  val age : Int, val tailSize: Double) extends amy(age)
+class doggy (override  val age : Int, val earlSize: Double) extends amy(age)
 
 object kutty extends catty(4, 2.33)
 object dogy extends doggy(4, 2.33)
@@ -14,7 +14,7 @@ object dogy extends doggy(4, 2.33)
 
 object ObjInh {
   def main(args: Array[String]): Unit={
-    println("kutty =  " + kutty.age + "More Data = " + kutty.tailSize )
+    println("kutty =  " + kutty.age + " More Data = " + kutty.tailSize )
     println("dogy  = " + dogy.age + " More Data  = " + dogy.earlSize)
   }
-}*/
+}
